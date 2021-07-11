234885e63fd7e140b6c658691e4f82f77b976722
Koji Oishi <39555285+jiko797torayo@users.noreply.github.com>
2021-07-12 08:41:57 +0900

    カスタムフック作成

--- src/App.tsx
+++ src/App.tsx
@@ -6,32 +6,7 @@ import { useState } from "react";
 import { UserProfile } from "./types/userProfile";
 
 export default function App() {
-  const [userProfiles, setUserProfiles] = useState<Array<UserProfile>>([]);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(false);
-
-  const onClickFetchUser = () => {
-    setLoading(true);
-    setError(false);
-
-    axios
-      .get<Array<User>>("https://jsonplaceholder.typicode.com/users")
-      .then((res) => {
-        const data = res.data.map((user) => ({
-          id: user.id,
-          name: user.name,
-          email: user.email,
-          address: `${user.address.city}${user.address.suite}${user.address.street}`
-        }));
-        setUserProfiles(data);
-      })
-      .catch(() => {
-        setError(true);
-      })
-      .finally(() => {
-        setLoading(false);
-      });
-  };
+  const onClickFetchUser = () => {};
 
   return (
     <div className="App">
